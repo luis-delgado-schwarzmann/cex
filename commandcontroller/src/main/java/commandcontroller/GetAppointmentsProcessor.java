0cdf454a2b46659ba4922af99148b051816b153a
Luis Delgado Schwarzmann <ldelgado@stratio.com>
2017-07-14 12:58:08 +0200

    First commit command controller

---
+++ commandcontroller/src/main/java/commandcontroller/GetAppointmentsProcessor.java
@@ -0,0 +1,14 @@
+package commandcontroller;
+
+public class GetAppointmentsProcessor {
+
+    private final String id;
+
+    public GetAppointmentsProcessor(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
