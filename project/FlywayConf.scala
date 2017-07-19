0017e3cac584a9945c30bde1815103d66e901436
Mario Fernández <mariofernandez@stratio.com>
2017-07-19 11:02:32 +0200

    Refactor of sbt projects for having dependency objects

--- project/FlywayConf.scala
+++ project/FlywayConf.scala
@@ -1,5 +1,10 @@
+import com.typesafe.config.Config
 import sbt._
 
 object FlywayConf {
   val conf = settingKey[Map[String, String]]("Flyway configuration")
+
+  def parse(file: Config, prefix: String, values: Seq[String]): Map[String, String] = {
+    values.foldLeft(Map[String, String]())((acc, v) => acc ++ Map(v -> file.getString(s"${prefix}.${v}")))
+  }
 }
